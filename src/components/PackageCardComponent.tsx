d41c4153077f95d8e39d5195a11889a3a7456206
katalyst33 <tyemail@gmail.com>
2022-06-18 04:57:08 +0100

    refactored drop down

--- src/components/PackageCardComponent.tsx
+++ src/components/PackageCardComponent.tsx
@@ -1,34 +1,24 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Debug from "./DebugDataComponent";
-import { Disclosure } from '@headlessui/react'
-import { BsChevronDown } from 'react-icons/bs'
-import {BsChevronLeft} from "react-icons/bs";
-import {BsChevronRight} from "react-icons/bs";
+import { Disclosure } from "@headlessui/react";
+import { BsChevronDown } from "react-icons/bs";
+import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronRight } from "react-icons/bs";
 import DropDown from "./DropDown";
 
-interface PackageServiceTypings {
-  title: string;
-  description: string;
-}
-
-interface PackageInfoTypings {
-  title: string;
-  description: string;
-  services: PackageServiceTypings[];
-  link: string;
-  duration: string;
-  regularCar: string;
-  largeCar: string;
-  logistics: string;
-}
-
-interface PackageCardTypings {
-  packageInfo: PackageInfoTypings;
-}
+import ServiceDropDownComponent from "./ServiceDropDownComponent";
+import {
+  PackageCardTypings,
+  PackageServiceTypings,
+} from "../typings/AllTypings";
 
 export default function PackageCardComponent(props: PackageCardTypings) {
   const packageInfo = props.packageInfo;
+  const [drop, setDrop] = React.useState(false);
+  function toggleDrop(index: any) {
+    setDrop(!drop);
+  }
 
   return (
     <div className="">
@@ -37,65 +27,46 @@ export default function PackageCardComponent(props: PackageCardTypings) {
         <h1 className="text-white text-3xl mt-2 mb-3">{packageInfo.title}</h1>
         <p className="text-white">{packageInfo.description}</p>
 
-        {/* <Disclosure>
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Interior</span>
-                <BsChevronDown
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-5 w-5 text-purple-500`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white border-b-1">
-                If you're unhappy with your purchase for any reason, email us
-                within 90 days and we'll refund you in full, no questions asked.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure>
-        <Disclosure as="div" className="mt-2">
-          {({ open }) => (
-            <>
-              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
-                <span>Exterior</span>
-                <BsChevronDown
-                  className={`${
-                    open ? 'rotate-180 transform' : ''
-                  } h-5 w-5 text-purple-500`}
-                />
-              </Disclosure.Button>
-              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-white border-b-1">
-                No.
-              </Disclosure.Panel>
-            </>
-          )}
-        </Disclosure> */}
-
         <div>
           <ul>
             {packageInfo.services.map(
               (service: PackageServiceTypings, index: number) => (
                 <div key={index}>
-                  <DropDown title = {service.title} description={service.description}/>
+                  <ServiceDropDownComponent service={service} />
                 </div>
               )
             )}
           </ul>
+
+          {/*   <ul>
+            {packageInfo.services.map(
+              (service: PackageServiceTypings, index: number) => (
+                <div key={index}>
+                  <DropDown
+                    title={service.title}
+                    description={service.description}
+                  />
+                </div>
+              )
+            )}
+          </ul> */}
         </div>
 
         <div>
           <h1 className="text-white">Duration: {packageInfo.duration}</h1>
-          <h1 className="text-white">Regular Car {packageInfo.regularCar}</h1>{" "}
+          <h1 className="text-white">
+            Regular Car {packageInfo.regularCar}
+          </h1>{" "}
           <h1 className="text-white">Large Car {packageInfo.largeCar}</h1>
           <p className="mt-8">
             F{" "}
-            <span className="text-xs text-white">
-              {packageInfo.logistics}
-            </span>
+            <span className="text-xs text-white">{packageInfo.logistics}</span>
           </p>
-          <div className="text-center my-4 "><button className="bg-slate-100 text-blue-600 rounded-xl px-20 py-2 shadow-md shadow-slate-900">I Want This</button></div>
+          <div className="text-center my-4 ">
+            <button className="bg-slate-100 text-blue-600 rounded-xl px-20 py-2 shadow-md shadow-slate-900">
+              I Want This
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/PackagesPage.tsx
+++ src/pages/PackagesPage.tsx
@@ -1,36 +1,13 @@
 import "../css/App.css";
 import Debug from "../components/DebugDataComponent";
 import PackageCardComponent from "../components/PackageCardComponent";
-import {BsChevronLeft} from "react-icons/bs";
-import {BsChevronRight} from "react-icons/bs";
+import { BsChevronLeft } from "react-icons/bs";
+import { BsChevronRight } from "react-icons/bs";
 
 function PackagesPage() {
   const allPackages = [
     {
-        title: "Golden",
-        description: "Popular package",
-        regularCar: "N43,830",
-        duration: "8 - 9 hours",
-        largeCar: "N53,830",
-        logistics: "5,500 extra Logistics fee to Mainland.",
-        services: [
-          {
-            title: "Exterior",
-            description: "Exterior cleaning",
-          },
-          {
-            title: "Interior",
-            description: "Interior cleaning",
-          },
-          {
-            title: "Engine",
-            description: "Engine cleaning",
-          },
-        ],
-        link: "golden-package",
-    },
-    {
-      title: "Silver",
+      title: "Golden",
       description: "Popular package",
       regularCar: "N43,830",
       duration: "8 - 9 hours",
@@ -99,30 +76,52 @@ function PackagesPage() {
       link: "golden-package",
     },
     {
-    title: "Diamond",
-    description: "Popular package",
-    regularCar: "N43,830",
-    duration: "8 - 9 hours",
-    largeCar: "N53,830",
-    logistics: "5,500 extra Logistics fee to Mainland.",
-    services: [
-      {
-        title: "Exterior",
-        description: "Exterior cleaning",
-      },
-      {
-        title: "Interior",
-        description: "Interior cleaning",
-      },
-      {
-        title: "Engine",
-        description: "Engine cleaning",
-      },
-      
-    ],
-    link: "golden-package",
-}
-  ]
+      title: "Silver",
+      description: "Popular package",
+      regularCar: "N43,830",
+      duration: "8 - 9 hours",
+      largeCar: "N53,830",
+      logistics: "5,500 extra Logistics fee to Mainland.",
+      services: [
+        {
+          title: "Exterior",
+          description: "Exterior cleaning",
+        },
+        {
+          title: "Interior",
+          description: "Interior cleaning",
+        },
+        {
+          title: "Engine",
+          description: "Engine cleaning",
+        },
+      ],
+      link: "golden-package",
+    },
+    {
+      title: "Diamond",
+      description: "Popular package",
+      regularCar: "N43,830",
+      duration: "8 - 9 hours",
+      largeCar: "N53,830",
+      logistics: "5,500 extra Logistics fee to Mainland.",
+      services: [
+        {
+          title: "Exterior",
+          description: "Exterior cleaning",
+        },
+        {
+          title: "Interior",
+          description: "Interior cleaning",
+        },
+        {
+          title: "Engine",
+          description: "Engine cleaning",
+        },
+      ],
+      link: "golden-package",
+    },
+  ];
   const packageData = {
     title: "Golden",
     description: "popular package",
@@ -163,18 +162,29 @@ function PackagesPage() {
           Choose a package below and get it <br /> delivered at your door step
         </h1> */}
         <h1 className="text-center text-xl lg:text-3xl py-20 ">
+          <i className="fa-solid fa-user"></i>
           Choose a package below and get it <br /> delivered at your door step
         </h1>
         {/* grid md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-10 w-5/6 mx-auto */}
         <section className="flex items-center">
-          <BsChevronLeft size={30} onClick={sliderleft} className="hidden lg:block  opacity-20 hover:opacity-100"/>
-          <div id="slider" className="mx-5 flex gap-8 w-full h-full overflow-x-scroll scroll-smooth scrollbar-hide">
-            
+          <BsChevronLeft
+            size={30}
+            onClick={sliderleft}
+            className="hidden lg:block  opacity-20 hover:opacity-100"
+          />
+          <div
+            id="slider"
+            className="mx-5 flex gap-8 w-full h-full overflow-x-scroll scroll-smooth scrollbar-hide"
+          >
             {allPackages.map((packageInfo, index) => (
               <PackageCardComponent key={index} packageInfo={packageInfo} />
             ))}
           </div>
-          <BsChevronRight className="lg:block hidden opacity-20 hover:opacity-100" size={30} onClick={slideright}/>
+          <BsChevronRight
+            className="lg:block hidden opacity-20 hover:opacity-100"
+            size={30}
+            onClick={slideright}
+          />
         </section>
       </div>
     </>
