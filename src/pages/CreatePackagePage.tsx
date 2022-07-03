1add0a793177fa8d42b0bde5e51b2af6a6b347dd
katalyst33 <tyemail@gmail.com>
2022-07-03 15:37:15 +0100

    add create new package endpoint

--- src/pages/CreatePackagePage.tsx
+++ src/pages/CreatePackagePage.tsx
@@ -1,4 +1,37 @@
+import axios from "axios";
+
 const CreatePackage = () => {
+  function addNewPackage(e: any) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5100/packages", {
+        id: "4",
+        title: "Vibranium Ultra",
+        description: "Best package",
+        regularCar: "N200,830",
+        duration: "24 hours",
+        largeCar: "N250,830",
+        logistics: "50,500 extra Logistics fee to Mainland.",
+        services: [
+          {
+            title: "Exterior",
+            description: "Exterior cleaning",
+          },
+          {
+            title: "Interior",
+            description: "Interior cleaning",
+          },
+        ],
+        link: "golden-package",
+      })
+      .then(() => {
+        console.log("a new package was created");
+      })
+      .catch((error) => {
+        console.log("package ws not created", error);
+      });
+  }
+
   return (
     <div className="mt-60">
       {JSON.stringify({
@@ -26,24 +59,30 @@ const CreatePackage = () => {
       })}
       <h1 className="text-lg">i am cr eate package page</h1>
 
-      <section>
-        <div>
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email
-          </label>
-          <div className="mt-1">
-            <input
-              type="email"
-              name="email"
-              id="email"
-              className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-              placeholder="you@example.com"
-            />
-          </div>
-        </div>
+      <section className="flex justify-center">
+        <form>
+          <main className="w-96">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700 text-left"
+              >
+                Title:
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                  placeholder="Package title"
+                />
+              </div>
+            </div>
+          </main>
+
+          <button className="bg-red-500" onClick={addNewPackage}>
+            Create Package
+          </button>
+        </form>
       </section>
     </div>
   );
